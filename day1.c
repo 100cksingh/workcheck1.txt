f2b95512de3ec31657b6ce27d5ff5ad3aaff4148
100cksingh <chandrakantsingh312@gmail.com>
2021-10-10 22:12:54 +0530

    innitial  commit in day1.c file using git commmand

---
+++ day1.c
@@ -0,0 +1,34 @@
+// This is a c language file created using vi command in git to vs code 
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("hello git command in c ");
+	printf("Having a nice project in this file using the git command so let's start");
+	return 0;
+}
+// day2.c
+
+
+
+
+
+rd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exit
+c:\>
